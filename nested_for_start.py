6244e4c3d499831d3c79178e6ff654229572bd53
kumarrkslinux <kumarrkslinux@gmail.com>
2021-09-13 10:08:27 +0530

    Update nested_for_start.py

--- nested_for_start.py
+++ nested_for_start.py
@@ -5,6 +5,6 @@ for number in range(count):
     for letter in range(count):
         print('', end= ' ' )
     for harshvalue in range(number+1):
-       print(chr(number+65), end=' ')
+       print(chr(letter+65), end=' ')
     print()
     count = count -1
