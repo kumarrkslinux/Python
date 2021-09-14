b7636caaca7ae596136249bba1e7e9bc4397ac92
root <root@centos8.kms.com>
2021-09-14 09:42:10 +0530

    immutable datatybes and compond datatypes

---
+++ immutable.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# change in memeory address not with value
+state1 = 'Tamil nadu'
+state2 = 'Tamil nadu'
+
+print (state1 is state2)
+print (id(state1))
+# is = compares the memeory address of the given identifier 
+
+print (state1 == state2)
+print (id(state2))
+# == compares the value of the given identifier
+# immutable datatybes
+#float, integer, boolen, string, byte, complex
---
+++ mutable.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+name = 'raja'
+name[1] = 'o'
+print(name)
