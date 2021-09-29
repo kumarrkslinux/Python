68e8844ec6d7e573237bad6919d2015f8e7496cc
kumarrkslinux <kumarrkslinux@gmail.com>
2021-09-29 09:38:12 +0530

    Update Difference.py

--- connection_study/Difference.py
+++ connection_study/Difference.py
@@ -1,4 +1,4 @@
 List is a collection which is ordered and changeable. Allows duplicate members. -- Mutable - [] 
 Tuple is a collection which is ordered and unchangeable. Allows duplicate members. -- Immutable - () 
 Set is a collection which is unordered and unindexed. No duplicate members. {} -- Immutable 
-Dictionary is a collection which is ordered* and changeable. No duplicate members. -- { key:value } or { items }
+Dictionary is a collection which is ordered* and changeable. No duplicate members. -- { key:value } or { items } - Key --> Uniqe, value --> can change
