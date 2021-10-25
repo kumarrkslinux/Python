c6a78e1fb6f1a0e9df04a6846a1aaaaf44d2d8ea
kumarrkslinux <kumarrkslinux@gmail.com>
2021-10-25 22:31:08 +0530

    Update csv_file_writing.py

--- Program/csv_file_writing.py
+++ Program/csv_file_writing.py
@@ -4,15 +4,15 @@ import csv
 
 with open("student.csv", 'w' ) as f:
     w = csv.writer(f)
-    w.writerow(['Name', 'number', 'Persentage', 'Department'])
+    w.writerow(['Name', 'Number', 'Persentage', 'Department'])
 
     while True: 
 
      name = input("Enter your name: " )
      Roll_number =int(input("Enter your Roll number: "))
-     Persentage = int(input("Enter your Persentage: "))
-     Department = input("Enter your Department; ")
-     w.writerow(['Name', 'number', 'Persentage', 'Department'])
+     Persen = int(input("Enter your Persentage: "))
+     Departm = input("Enter your Department; ")
+     w.writerow(['name', 'Roll_number', 'Persen', 'Departm'])
      option = input("continue? Yes|No: ")
      if option.lower() == 'no':
          break
