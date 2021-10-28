efa4f915e9f9c12f6ab27f34f909f71d9c4903c3
root <root@centos8.kms.com>
2021-10-28 10:47:06 +0530

    OOP programming

---
+++ oop/class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.6
+
+class SuperMarket:
+      pass
+
+production1 = SuperMarket()
+
+production1.name = 'coffeee'
+production1.prize = 20
+production1.dicount = 1.02
+
+production2 = SuperMarket()
+
+production2.name = 'tea'
+production2.prize = 21
+production1.dicount = 1.12
+
+
+print(production1)
+print(production2)
+
---
+++ oop/oop_study.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3.6
+
+
+#object --> state, behaviour  1 state --> 1 behaviour --> function --> method 
+
+#How many objects can be in the class ?
+
+#Practically we need min 1 object, and Max there, is no restiction  
+
+#Where we keep object and fucntion that place called clause. 
+
+#one name memeory space called class 
+
+#class -> template, behaviour
+
+
+#variable -> atrributes 
+#function -> method
+#objects -> instances
+
+#class vodafone: # memeory space Document Name
+
+#executiove = vodafone() 
+
+#manager = vodafone()   
+
+#object name 
+#representative referecence name of the object 
+#object represents class
+#object is instance of class 
+
+# class variable  - is applicable for class and all the instance present inside the class. 
+# instance variable - For each instance, memeory copy will be created 
+
+# contructor - is useful for initialising object sepcific information .... Contructor will be called automatically whenevet you creating objects
